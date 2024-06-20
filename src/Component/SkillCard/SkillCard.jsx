f23846375590a90fee02127f7d918f30e7c904f6
seba ghnem <s.sseba96@gmail.com>
2024-06-20 23:57:40 +0300

    update skills mode

--- src/Component/SkillCard/SkillCard.jsx
+++ src/Component/SkillCard/SkillCard.jsx
@@ -1,12 +1,16 @@
+import React, {useState, useContext} from 'react';
 import './SkillCard.css'
+import {DarkModeContext} from './../../context/DarkModeContext';
 
 export default function SkillCard({ skill,  progress}) {
-  
+  //============================= Mode =======================
+  const theme= localStorage.getItem("theme");
   return (
     <>
-      <div className="skillCard flex-center flex-col">
-        <p>{skill}</p>
-        <div class="progress-div" data-progress={progress} style={{"--progress": progress }}>{progress}</div>    
+     {/*  <div className={`skillCard flex-center flex-col ${theme ==="dark" ?  'dark' : ''}`}> */}
+     <div className={theme ==="light" ? 'skillCard flex-center flex-col light' : 'skillCard flex-center flex-col'}> 
+        <p class="skill-title">{skill}</p>
+        <p class="progress-div" data-progress={progress} style={{"--progress": progress }}>{progress}</p>    
       </div>
     </>
   );
