b99438ef6b0a3bd0371442748cf82e1bde1a7bf1
seba ghnem <s.sseba96@gmail.com>
2024-08-10 18:13:36 +0300

    add vica certificate

--- src/Component/Achievements/Achievements.jsx
+++ src/Component/Achievements/Achievements.jsx
@@ -2,6 +2,13 @@ import React , {useContext} from 'react'
 import './Achievements.css'
 import Title from './../Title/Title'
 import {LangContext} from './../../i18n';
+import Vica_certificate from './../../assets/images/vica_certificate.jpg'
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Achievements() {
   const { t } = useContext(LangContext);
@@ -11,21 +18,33 @@ export default function Achievements() {
     <div className={`containerSection achievements secBackground ${lang ==='ar' ? 'arabic' : ''}`}  id="achievements">
       <div className="background_black"></div>
       <Title nameClass="titlePosition" title={t("navbar.Achievements and Certificates")} />
-      <div>
-      <ul className="flex-col">
-        <li>
-           <p className="education_achievements_desc flex-col">Zero Web School trainee
-             <span>{t("achievements.achievement_1_date")}</span>
-           </p>
-        </li>
-        <li>
-            <p className="education_achievements_desc flex-col">Online Front-end trainee
-              <span style={{direction: "ltr"}}>{t("achievements.achievement_2_date")}</span>
-            </p>
-        </li>
-      </ul>
-       
-       
+      <div className='achievement_swiper'>
+        <Swiper
+            modules={[Autoplay, Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={1}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{
+                delay:1500,
+                disabledInteraction: false
+              }}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+          >
+          <SwiperSlide>
+            <div className="card">
+              <img className='card-img-top' src={Vica_certificate} />
+              <div className='card-body'>
+                <p className='card-text'>
+                  {t("achievements.achievement_vica")}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   )
