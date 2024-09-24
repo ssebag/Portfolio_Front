db6d0d892befc7e67f1d6dba716828d4e26c1897
unknown <s.sseba96@gmail.com>
2024-09-24 00:17:18 -0700

    add notification for contact

--- src/Component/Contactme/Contactme.jsx
+++ src/Component/Contactme/Contactme.jsx
@@ -1,8 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import './Contactme.css'
 export const Contactme = () => {
   const form = useRef();
+  /* =================== empty field =============== */
+  const [formData, setFormData] = useState({
+    from_name: '',
+    from_email: '',
+    message: '',
+  });
+  /* =================== update status when change value of feild ================= */
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  /* ================== notification ================ */
+  const [notification, setNotification] = useState('');
+  const [showNotification, setShowNotification] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -14,21 +28,38 @@ export const Contactme = () => {
       .then(
         () => {
           console.log('SUCCESS!');
+          setFormData({ from_name: '', from_email: '', message: '' });
+          setNotification('SUCCESS');
+          setShowNotification(true);
         },
         (error) => {
           console.log('FAILED...', error.text);
+          setNotification('FAILD. TRY AGAIN');
+          setShowNotification(true);
         },
       );
   };
+  useEffect(() => {
+    if (showNotification) {
+      const timer = setTimeout(() => {
+        setShowNotification(false);
+      }, 5000); 
 
+      return () => clearTimeout(timer);
+    }
+  }, [showNotification]);
   return (
    <div className="contactMe">
       <form className='flex-col' ref={form} onSubmit={sendEmail}>
-        <input className='name' type="text" name="from_name" placeholder='Your Name' />
-        <input className='email' type="email" name="from_email" placeholder='Your Email' />
-        <textarea className='message' name="message" placeholder='Your Message' />
+        <input className='name' type="text" name="from_name" placeholder='Your Name' 
+          value={formData.from_name} onChange={handleChange} required />
+        <input className='email' type="email" name="from_email" placeholder='Your Email'
+         value={formData.from_email} onChange={handleChange} required />
+        <textarea className='message' name="message" placeholder='Your Message' 
+         value={formData.message} onChange={handleChange} required/>
         <input className='submitButton' type="submit" value="Send" />
       </form>
+      {notification && <div style={{ transition: 'opacity 0.5s', opacity: showNotification ? 1 : 0 }} className='notification'>{notification}</div>}
    </div>
   );
 };
